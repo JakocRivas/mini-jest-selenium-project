4c2bce441aebb7384f5808d9480d5a715b532406
Jakoc Rivas <jrivas@intellisys.com.do>
2019-10-18 15:06:11 -0400

    deletes useless comment on env.js

--- resources/configuration/env.js
+++ resources/configuration/env.js
@@ -1,9 +1,1 @@
 require("dotenv").config();
-// module.exports = {
-//   //   endpoint: process.env.API_URL,
-//   //   masterKey: process.env.API_KEY,
-//   //   port: process.env.PORT,
-//   email: process.env.email,
-//   password: process.env.password,
-//   url: process.env.ROOT_URL
-// };
